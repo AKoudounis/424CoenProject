eb50d79620c06639f6b11ef07d58a2027d9cffa9
AKoudounis <andreaskoudounis@gmail.com>
2024-11-07 03:11:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def upload_file():
             # Upload the file directly to the Cloud Storage bucket
             blob = storage_client.bucket(BUCKET_NAME).blob(file.filename)
             blob.upload_from_file(file)
+            time.sleep(5)  # Adjust this delay based on your needs
+
 
             # Process the file (pass the Cloud Storage filename)
             process_file(blob.name)  # Use the blob name (file name)
